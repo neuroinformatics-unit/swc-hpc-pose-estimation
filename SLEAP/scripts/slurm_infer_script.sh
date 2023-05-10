d3081006e37364c658e75c5cc3ba5bd2f982a0a1
niksirbi <niko.sirbiladze@gmail.com>
2023-05-10 16:45:55 +0100

    updated sleap inference script

--- SLEAP/scripts/slurm_infer_script.sh
+++ SLEAP/scripts/slurm_infer_script.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 1   # number of nodes
 #SBATCH --mem 12G # memory pool for all cores
 #SBATCH -n 2 # number of cores
-#SBATCH -t 0-01:00 # time (D-HH:MM)
+#SBATCH -t 0-02:00 # time (D-HH:MM)
 #SBATCH --gres gpu:1 # request 1 GPU (of any kind)
 #SBATCH -o slurm.%N.%j.out # STDOUT
 #SBATCH -e slurm.%N.%j.err # STDERR
@@ -26,6 +26,8 @@ cd $JOB_DIR
 sleap-track $DATA_DIR/videos/M708149_EPM_20200317_165049331-converted.mp4 \
     -m $JOB_DIR/models/230509_141357.centroid/training_config.json \
     -m $JOB_DIR/models/230509_141357.centered_instance/training_config.json \
-    --tracking.tracker none -o labels.v001.slp.predictions.slp \
+    --gpu auto \
+    --tracking.tracker none \
+    -o labels.v001.slp.predictions.slp \
     --verbosity json \
     --no-empty-frames
