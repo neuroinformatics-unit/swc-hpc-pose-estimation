1241c7a86eab535f60ede0210c28c19269908bb8
Niko Sirmpilatze <niko.sirbiladze@gmail.com>
2023-05-14 15:09:06 +0100

    Fix sleap python scripts (#2)
    * fixed errors in python scripts for SLEAP training
    
    * added extra period in prefix

--- SLEAP/scripts/run_sleap_training.py
+++ SLEAP/scripts/run_sleap_training.py
@@ -22,28 +22,26 @@ def main(batch_size=4):
         Applies to both centroid and centered instance models
         of the top-down configuration.
     """
-    training_job = SLEAPTrainer_TopDown_SingleInstance(
-        train_dir=TEST_DATA_DIR / "labels.v002.slp.array_training",
-        labels_path=TEST_DATA_DIR / "labels.v002.slp.training_job/labels.v002.pkg.slp",
-        skeleton_path=TEST_DATA_DIR / "skeleton.json",
-        train_fraction=0.8,
-        run_name_prefix=f"batch-size-{batch_size}",
-        anchor_part="centre",
-        camera_view="top",
-    )
-    training_job.configure_centroid_model(
-        input_scaling=0.5,
-        n_epochs=50,
-        batch_size=batch_size,
-        max_stride=32,
-    )
-    training_job.configure_centered_instance_model(
-        input_scaling=1.0,
-        n_epochs=100,
-        batch_size=batch_size,
-        max_stride=32,
-    )
-    training_job.train_models()
+
+    for model_type in ["centroid", "centered_instance"]:
+        training_job = SLEAPTrainer_TopDown_SingleInstance(
+            train_dir=TEST_DATA_DIR / "labels.v002.slp.array_training",
+            labels_path=TEST_DATA_DIR
+            / "labels.v002.slp.training_job/labels.v002.pkg.slp",
+            skeleton_path=TEST_DATA_DIR / "skeleton.json",
+            train_fraction=0.8,
+            run_name_prefix=f"batch-size-{batch_size}.",
+            anchor_part="centre",
+            camera_view="top",
+        )
+        training_job.configure_model(
+            model_type=model_type,
+            input_scaling=1.0,
+            n_epochs=100,
+            batch_size=batch_size,
+            max_stride=32,
+        )
+        training_job.train_model()
 
 
 if __name__ == "__main__":
